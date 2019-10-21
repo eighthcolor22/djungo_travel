69b1db9b4c867e4928f51ebe36e251c5858a849e
eighthcolor22 <eighthcolor22@gmail.com>
2019-10-21 12:09:51 +0300

    # выводятся поезда удовлетворяющие запросу в форме

--- routes/views.py
+++ routes/views.py
@@ -46,14 +46,25 @@ def find_routes(request):
             data = form.cleaned_data  # проверить, объект типа словарь
             # assert False
             from_city = data['from_city']
+
+            # print('\n','from_city', from_city)
+
             to_city = data['to_city']
             across_cities_form = data['across_cities']  # получаем что-то в виде списка
             traveling_time = data['traveling_time']
             # на данном этапе мы получили все данные с формы
 
             graph = get_graph()
+
+            # print()
+            # print('our_graph', graph)
+            # print()
+
             all_ways = list(dfs_paths(graph, from_city.id, to_city.id))  # список
             # всех маршрутов между выбранными в форме городами
+            # print()
+            # print('all_ways', all_ways)
+            # print()
 
             # проверка на наличие маршрутов между указанными городами в форме
             if len(all_ways) == 0:
@@ -84,20 +95,59 @@ def find_routes(request):
             #  конечные города нам подходят
             else:
                 right_ways = all_ways
+        # на данный момент мы получаем список, содержащий ID городов входящих в удовлетворяющие маршруты.
 
-        # отображаем эту дичь
+        # на основе списка right_ways выбираем поезда, которые включены в наш маршрут, чтоб их отобразить
         trains = []
         for route in right_ways:
             tmp = {}
-            tmp
+            tmp['trains'] = []
+            total_time = 0
+            for index in range(len(route)-1):
+                qs = Train.objects.filter(from_city=route[index], to_city=route[index+1])  # запрос, возвращающий
+                # поезда(с модели Train), выбранные по значениям из маршрутов
+                qs = qs.order_by('travel_time').first()  # конструкция фильтрует все значения поездов и
+                # возвращает с намиеньшим значение поля'travel_time'
+                total_time += qs.travel_time
+                tmp['trains'].append(qs)
+            tmp['total_time'] = total_time  # полное время пути
 
+            # проверка соответствия времени
+            if total_time <= traveling_time:
+                trains.append(tmp)
+        if not trains:
+            messages.error(request, 'время в пути больше заданного')
+            return render(request, 'routes/home.html', {'form': form})
+
+        routes = []
+        cities = {'from_city': from_city.name, 'to_city': to_city.name}
+        for tr in trains:
+            routes.append({'route': tr['trains'],
+                           'total_time': tr['total_time'],
+                           'from_city': from_city.name,
+                           'to_city': to_city.name
+                           })
+
+        sorted_routes = []
+        if len(routes) == 1:
+            sorted_routes = routes
+        else:
+            times = list(set(x['total_time'] for x in routes))
+            times = sorted(times)
+            for time in times:
+                for route in routes:
+                    if time == route['total_time']:
+                        sorted_routes.append(route)
+
+        # отображаем эту дичь
         context = {}
         form = RouteForm()
         context['form'] = form
-        context['ways'] = right_ways
+        context['routes'] = sorted_routes
+        context['cities'] = cities
         return render(request, 'routes/home.html', context)
 
-        return render(request, 'routes/home.html', {'form': form})
+        # return render(request, 'routes/home.html', {'form': form})
     else:
         messages.error(request, 'Создайте маршрут')
         form = RouteForm()
